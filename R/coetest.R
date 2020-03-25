470ba690b14535e65d94c853a4600746622ff9d2
mrzdcmps <moritz.dechamps@gmail.com>
2020-03-25 13:37:17 +0100

    changed output of fftlikelihood

--- R/coetest.R
+++ R/coetest.R
@@ -167,7 +167,8 @@ fftlikelihood <- function(df, proportion = 100, sims.df = sims, sims.df.col = "d
   }
   stopCluster(cl)
   
-  cat("\nLikelihood for",sum(df$LowerSims > 0.95),"(=",sum(df$LowerSims > 0.95)/nrow(df),"%) or more Top5-Frequencies is estimated",(sum(likelihoodlist >= sum(df$LowerSims > 0.95))/length(likelihoodlist))*100,"% \n")
+  cat("\nLikelihood for",sum(df$LowerSims > 0.95),"(=",(sum(df$LowerSims > 0.95)/nrow(df))*100,"%) or more Top5-Frequencies is estimated",(sum(likelihoodlist >= sum(df$LowerSims > 0.95))/length(likelihoodlist))*100,"% \n")
   
-  return(likelihoodlist)
+  likelihood.out <- list("Likelihood" = sum(likelihoodlist >= sum(df$LowerSims > 0.95))/length(likelihoodlist), "Top5 of Sims" = likelihoodlist)
+  return(likelihood.out)
 }
