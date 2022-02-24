d10ffdb34c53f6e062478fc5bc87d2beb6a75b64
mrzdcmps <moritz.dechamps@gmail.com>
2022-02-24 13:55:35 +0100

    added support for independent samples t-test

--- R/bayestest.R
+++ R/bayestest.R
@@ -58,46 +58,80 @@ bfbinom <- function(data, p = 0.5, prior.r = 0.1, nullInterval = NULL, nstart = 
 #' The function first calculates t-scores and p-values and subsequently uses the BFDA package to translate the data into BFs.
 #' Returns a list of t-scores ($t-value), p-scores ($p-value), and BF ($BF)
 #'
-#' @param data A vector containing data values.
-#' @param ydata A vector containing another set of values for a paired samples test (independent samples are currently not supported). Please indicate with paired = TRUE.
+#' @param x A vector containing data values.
+#' @param y A vector containing another set of values for a paired samples test.
+#' @param formula A formula of the form var ~ group where var is a numeric variable giving the data values and group is indicating which group the data point belongs to (must be 2 levels). Use this for an independent samples t-Test.
+#' @param data Use with formula. A data frame containing the variables given in the formula. Use this for an independent samples t-Test.
 #' @param alternative Indicates the direction of the alternative hypothesis: two.sided, less, or greater
 #' @param mu A number indicating the true value of the mean (or difference in means if you are performing a two sample test).
-#' @param paired Logical. Set TRUE if you want to perform a paired samples t-Test.
 #' @param prior.loc Location of the cauchy distributed prior function (use 0 for an uninformed prior).
 #' @param prior.r Scale of thr cauchy distributed prior function.
 #' @param nstart How many data points should be considered before calculating the first BF (min = 2).
 #' @examples
-#' bflist <- bfttest(sumscores, alternative = "greater")
+#' bfttest(sumscores, alternative = "greater") # One-sample
 #'
 #' tbl$bf <- bfttest(tbl$sums, alternative = "two.sided", mu = 50, nstart = 10, prior.loc = 0.1, prior.r = 0.05)$BF
+#' 
+#' bfttest(formula = raw~group, data = df) # Independent samples
+#' 
+#' bfttest(df1$scores, df2$scores) # Paired samples
 #' @export
 
-bfttest <- function(data, ydata = NULL, alternative = c("two.sided", "less", "greater"), mu = 0, paired = FALSE, prior.loc = 0, prior.r = 0.1, nstart = 5){
+bfttest <- function(x=NULL, y = NULL, formula = NULL, data = NULL, alternative = c("two.sided", "less", "greater"), mu = 0, prior.loc = 0, prior.r = 0.1, nstart = 5){
   # calculate t-scores and BFs
   bf <- t <- list()
-  data <- na.omit(data)
-  cat("N =",length(data),"\n")
-  cat("Calculating Sequential Bayes Factors...\n")
-  pb = txtProgressBar(min = nstart, max = length(data), style = 3)
-  if (!is.null(ydata)){
-    ydata <- na.omit(ydata)
-    if (length(ydata) != length(data)) stop("Data are not the same length!")
-    if (paired == TRUE) { # Paired Samples Test
-      for (i in nstart:length(data)) {
-        t[[i]] <- t.test(data[1:i], ydata[1:i], alternative = alternative, paired = T)
+  
+  if(length(alternative) > 1){
+    warning("No alternative specified. Using a two-sided alternative.")
+    alternative <- alternative[1]
+  }
+  
+  if (!is.null(formula)){ #Independent Samples
+    if(!is.null(x)) stop("Please use formula and data for independent and x (and y) for one-sample or paired samples tests.")
+    if(is.null(data)) stop("Please specify data.")
+    if(length(unique(data[,deparse(formula[[3]])])) != 2) stop("Group must have 2 levels.")
+    type <- "independent" 
+    samplesize <- nrow(data)
+    
+    cat("Independent Samples test (N = ",nrow(data),")\nCalculating Sequential Bayes Factors...\n",sep="")
+    pb = txtProgressBar(min = nstart, max = nrow(data), style = 3)
+    for (i in nstart:nrow(data)) {
+      t[[i]] <- t.test(formula=formula, data=data[1:i,], alternative = alternative, paired=F, var.equal=TRUE)
+      n1 <- table(data[1:i,deparse(formula[[3]])])[1]
+      n2 <- table(data[1:i,deparse(formula[[3]])])[2]
+      bf[[i]] <- bf10_t(t = t[[i]][[1]], n1 = n1, n2 = n2, independentSamples = T, prior.location = prior.loc, prior.scale = prior.r, prior.df = 1)
+      setTxtProgressBar(pb,i)
+    }
+  } else {
+    if(is.null(x)) stop("Please use formula and data for independent and x (and y) for one-sample or paired samples tests.")
+    x <- na.omit(x)
+    pb = txtProgressBar(min = nstart, max = length(x), style = 3)
+    
+    if (!is.null(y)){ # Paired Samples Test
+      y <- na.omit(y)
+      if (length(y) != length(x)) stop("Data are not the same length!")
+      type <- "paired"
+      samplesize <- length(x)
+      
+      cat("Paired Samples test (N = ",length(x),")\nCalculating Sequential Bayes Factors...\n",sep="")
+      for (i in nstart:length(x)) {
+        t[[i]] <- t.test(x[1:i], y[1:i], alternative = alternative, paired = T, var.equal=TRUE)
         bf[[i]] <- bf10_t(t = t[[i]][[1]], n1 = i, independentSamples = F, prior.location = prior.loc, prior.scale = prior.r, prior.df = 1)
         setTxtProgressBar(pb,i)
       }
-    } else { # Independent Samples Test
-        stop("Independent Samples t-Test currently not supported!")
-    }
-  } else { # One Sample Test
-    if(var(data[1:nstart]) == 0) stop("Cannot compute t-Test since there is no variance in the data. Please choose a larger nstart!")
-      for (i in nstart:length(data)) {
-        t[[i]] <- t.test(data[1:i], alternative = alternative, mu = mu)
+      
+    } else { # One Sample Test
+      if(var(x[1:nstart]) == 0) stop("Cannot compute t-Test since there is no variance in the data. Please choose a larger nstart!")
+      type <- "one-sample"
+      samplesize <- length(x)
+      
+      cat("One Sample test (N = ",length(x),")\nCalculating Sequential Bayes Factors...\n",sep="")
+      for (i in nstart:length(x)) {
+        t[[i]] <- t.test(x[1:i], alternative = alternative, mu = mu)
         bf[[i]] <- bf10_t(t = t[[i]][[1]], n1 = i, prior.location = prior.loc, prior.scale = prior.r, prior.df = 1)
         setTxtProgressBar(pb,i)
       }
+    }
   }
   
   close(pb)
@@ -110,13 +144,13 @@ bfttest <- function(data, ydata = NULL, alternative = c("two.sided", "less", "gr
   BFmin0 <- c(rep(1,(nstart-1)),unlist(lapply(bf, `[[`, 3)))
   
   if (alternative=="less"){
-    bft.out <- list("t-value" = unname(tlist), "p-value" = unname(plist), "BF" = unname(BFmin0))
+    bft.out <- list("t-value" = unname(tlist), "p-value" = unname(plist), "BF" = unname(BFmin0), "test type" = type, "prior" = list("Cauchy", "prior location" = prior.loc, "prior scale" = prior.r), "sample size" = samplesize, "alternative" = alternative)
   } else if (alternative=="greater"){
-    bft.out <- list("t-value" = unname(tlist), "p-value" = unname(plist), "BF" = unname(BFplus0))
+    bft.out <- list("t-value" = unname(tlist), "p-value" = unname(plist), "BF" = unname(BFplus0), "test type" = type, "prior" = list("Cauchy", "prior location" = prior.loc, "prior scale" = prior.r), "sample size" = samplesize, "alternative" = alternative)
   } else {
-    bft.out <- list("t-value" = unname(tlist), "p-value" = unname(plist), "BF" = unname(BF10))
+    bft.out <- list("t-value" = unname(tlist), "p-value" = unname(plist), "BF" = unname(BF10), "test type" = type, "prior" = list("Cauchy", "prior location" = prior.loc, "prior scale" = prior.r), "sample size" = samplesize, "alternative" = alternative)
   }
-  cat("Final Bayes Factor: ",tail(bft.out$BF,n=1)," (t=",tail(bft.out$`t-value`,n=1),"; p=",tail(bft.out$`p-value`,n=1),")",sep="")
+  cat("Final Bayes Factor: ",tail(bft.out$BF,n=1)," (t=",tail(bft.out$`t-value`,n=1),"; p=",tail(bft.out$`p-value`,n=1),")\n",sep="")
   return(bft.out)
 }
 
